c47769f6b1f1129adc1eb793ddeb3936bf351166
Nathan Center <nathanjcenter@gmail.com>
2022-03-15 14:03:54 -0500

    added robber skill set to the code

---
+++ IRobber.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Heist2
+{
+public interface IRobber
+{
+ string Name {get;set;}
+ int SkillLevel{get;set;}
+ int PercentageCut{get;set;}
+
+ void PerformSkill (Bank bank)
+ {
+     
+ }  
+}
+
+}
---
+++ LockSpecialist.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Heist2
+{
+    public class LockSpecialist:IRobber
+    {
+    public string Name {get;set;}
+    public int SkillLevel{get;set;}
+    public int PercentageCut{get;set;}
+
+     void PerformSkill (Bank bank)
+        {
+              bank.VaultScore -= SkillLevel;
+            Console.WriteLine($"{Name } is cracking the code with a   ${SkillLevel}");
+            if(bank.VaultScore<=0)
+            {
+                    Console.WriteLine($"{Name} has opened the vualt lets see whats inside!");
+            }
+        }
+    }
+
+}
